00f109d946b040fe8e7868de95ba1f2344edafee
eysarh <k_q_j@outlook.com>
2019-05-19 12:27:48 +0800

    测试获取源码

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,31 +1,66 @@
 <template>
-  <div class="test center-div">
+  <div v-if="ready" class="test center-div">
+    <codeEditor style="width: 100%;" :codeInfo.sync='data.codeInfo' ></codeEditor>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
+import codeEditor from './common/codeEditor.vue'
 export default {
   name: 'test',
   props: {
   },
   components: {
+    codeEditor,
+    // displayContent,
   },
   data () {
     return {
+      data: {
+        codeInfo: {
+          code: '',
+          lang: 'java',
+        },
+      },
+      ready: false,
+      
     }
   },
   methods: {
     async init () {
       
-			const sleep = (ms) => {
-				return new Promise(resolve => setTimeout(resolve, ms))
-      }
-      await sleep(1000)
+      // this.$store.dispatch('n', {
+      //   flag: 0,
+      //   method: 'get',
+      //   url: `/program-problems`,
+      //   params: {
+      //     page_num: this.params.program.page_num,
+      //     page_size: this.params.program.page_size,
+      //     difficult: this.params.program.difficult,
+      //     query: this.filter.keyword,
+      //     tagList: this.params.program.tagList,
+      //     uid: this.$_env.testUserInfo.uid,
+      //   }
+      // })
+      // const s = 'https://cdn.eagleoj.com/2019/4/18/712bcfe0-af33-47c2-beff-e11cbf8a52e9.java'
+      const s = '/noLogin/readImageFile'
+      let d = await axios.get(s)
+      console.log('d is ', d.data);
+      this.data.codeInfo.code = d.data
+			// const sleep = (ms) => {
+			// 	return new Promise(resolve => setTimeout(resolve, ms))
+      // }
+
+      // localhost:8080/noLogin/readImageFile
+      // await sleep(1000)
+      // https://cdn.eagleoj.com/2019/4/18/712bcfe0-af33-47c2-beff-e11cbf8a52e9.java
       this.ready = true
     }
   },
   created () {
-    // this.init()
+    this.init()
   }
 }
 </script>
