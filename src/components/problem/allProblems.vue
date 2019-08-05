d58120bad62a1219917180eb76927fb75e545ad6
eysarh <k_q_j@outlook.com>
2019-08-05 21:56:17 +0800

    重构部分代码，准备完成选择题部分

--- src/components/problem/allProblems.vue
+++ src/components/problem/allProblems.vue
@@ -1,4 +1,8 @@
 <template>
+  <!-- 
+    这个文件与子组件的耦合度极高，如果要改，建议重构
+    实在不想承认，这是因为年轻而犯下的错
+  -->
   <div class="allProblems main-view center-div">
     <div class="center-item">
       <div class="display">
@@ -14,8 +18,7 @@
             <div v-if="active===0">
               <div v-if='subReady.codeProblemsList'>
                 <div style="width: 100%;">
-                  
-                    <!-- row-style="background-color: #bebebe;" -->
+                  <!-- row-style="background-color: #bebebe;" -->
                   <el-table
                     :data="data.codeProblemsList.list"
                     style="width: 100%">
@@ -82,72 +85,8 @@
               </div>
             </div>
             <div v-if="active===1">
-              <div v-if='subReady.codeProblemsList'>
-                <div style="width: 100%;">
-                    <!-- row-style="background-color: #bebebe;" -->
-                  <el-table
-                    :data="data.codeProblemsList.list"
-                    style="width: 100%">
-                    <el-table-column
-                      width="70"
-                      v-if="this.$_env.testUserInfo.uid!==null"
-                      prop="name"
-                      label="状态">
-                      <template slot-scope="scope">
-                        <span class="accepted table-link" v-if="scope.row.status==='AC'">
-                          <i class="el-icon-check" style="color: rgb(25, 190, 107);"></i>
-                        </span>
-                        <span class="unaccepted table-link" v-if="scope.row.status===2">
-                          <i class="el-icon-minus" style="color: rgb(255, 153, 0);"></i>
-                        </span>
-                      </template>
-                    </el-table-column>
-                    <el-table-column
-                      label="标题"
-                      width="350">
-                      <template slot-scope="scope">
-                        <el-button type="text" @click="toProblem(scope.row)">{{scope.row.title}}</el-button>
-                      </template>
-                    </el-table-column>
-                    <el-table-column
-                      prop="runtime"
-                      label="提交次数">
-                      <template slot-scope="scope">
-                        <span>{{scope.row.submit_times}}</span>
-                      </template>
-                    </el-table-column>
-                    <el-table-column
-                      prop="name"
-                      label="通过率">
-                      <template slot-scope="scope">
-                        <span>{{ scope.row._percentage }}%</span>
-                      </template>
-                    </el-table-column>
-                    <el-table-column
-                      prop="name"
-                      label="难度">
-                      <template slot-scope="scope">
-                        <el-tag size='small' :type="scope.row._difficultFlag">{{scope.row._difficult}}</el-tag>
-                      </template>
-                    </el-table-column>
-                  </el-table>
-                </div>
-                <div class="bottom-control">
-                  <el-pagination
-                    @size-change="handleSizeChange"
-                    @current-change="handleCurrentChange"
-                    :current-page="params.program.page_num"
-                    :page-sizes="options.pageSize"
-                    :page-size="100"
-                    layout="total, sizes, prev, pager, next, jumper"
-                    :total="data.codeProblemsList.total">
-                  </el-pagination>
-                </div>
-              </div>
-              <div v-else class="history">
-                <div style="height: 100%;width: 100%;">
-                  <div v-for="item in data.unready" :key='item' class="skeleton-screen skeleton-item"></div>
-                </div>
+              <div v-if='subReady.completionProblemsList' style="width: 100%;">
+                <completion-problems :tag="filter.auxiTagList"></completion-problems>
               </div>
             </div>
           </div>
@@ -164,12 +103,6 @@
           </div>
           <div class="filter-row-tag">
             <div v-if="subReady.filter">
-              <!-- <mu-chip @click="handleTagClick(item)" class="item" :color="`${item.deleted==0?'#ffffff':'#e6e6e6'}`" 
-                :text-color="`${item.selected?'#2d8cf0':'#333333'}`"
-                v-for="(item, idx) in filter.tag" :key='idx' 
-                round :ripple='false'>
-                <span class="tag-content">{{item.tag_name}}</span><mu-badge :content="`${item.used_times}`" color="#61a9a7"></mu-badge>
-              </mu-chip> :type="`${item.deleted==0?'primary':'s'}`"-->
               <el-badge class="tag-badge" v-for="(item, idx) in filter.tag" :key='idx' 
                 :value="item.used_times" type="primary">
                 <el-tag @click="handleTagClick(item)" :class="`${item.isSelected?'tag-selected':'tag-unselected'}`">
@@ -191,15 +124,15 @@
 </template>
 
 <script>
-import codeProblemsList from './codeProblemsList.vue'
 import copyright from '../common/copyright.vue'
+import completionProblems from './completionProblems.vue'
 export default {
   name: 'allProblems',
   props: {
   },
   components: {
-    codeProblemsList,
     copyright,
+    completionProblems,
   },
   data () {
     return {
@@ -261,6 +194,7 @@ export default {
         keyword: '',
         tag: [],
         selectedTat: [],
+        auxiTagList: [],
       },
       active: 0,
       talbe: {
@@ -298,6 +232,9 @@ export default {
     }
   },
   methods: {
+    testc () {
+      this.test.push(5)
+    },
     async init () {
 			// const sleep = (ms) => {
 			// 	return new Promise(resolve => setTimeout(resolve, ms))
@@ -347,19 +284,12 @@ export default {
       this.subReady.screen = false
     },
     async getData () {
-      // 处理标签参数
-      let tagTemp = []
-      for (const i of this.filter.tag) {
-        if (i.isSelected) {
-          tagTemp.push(i.id)
-        }
-      }
       switch (this.active) {
         case 0: {
           this.data.codeProblemsList.list = []
           this.subReady.codeProblemsList = false
 
-          this.params.program.tagList = tagTemp.length===0?null:tagTemp.join(',')
+          this.params.program.tagList = this.filter.auxiTagList.length===0?null:this.filter.auxiTagList.join(',')
           await Promise.all([
             this.$store.dispatch('n', {
               flag: 0,
@@ -390,67 +320,10 @@ export default {
           break;
         }
         case 1: {
-          this.data.completionProblemsList.list = []
-          this.subReady.completionProblemsList = false
-
-          this.params.program.tagList = tagTemp.length===0?null:tagTemp.join(',')
-          await Promise.all([
-            this.$store.dispatch('n', {
-              flag: 1,
-              method: 'get',
-              url: `/object-problems`,
-              params: {
-                page_num: this.params.program.page_num,
-                page_size: this.params.program.page_size,
-                query: this.filter.keyword==""?null:this.filter.keyword,
-                tag_list: this.params.program.tagList,
-                uid: this.$_env.testUserInfo.uid,
-              }
-            }),
-          ])
-          if (!this.$store.state.n[1].success) return
-          this.data.codeProblemsList.list = this.$store.state.n[0].data.data
-          this.data.codeProblemsList.total = this.$store.state.n[0].data.total
-          this.$nextTick(function () {
-            this.subReady.codeProblemsList = true
-          })
-          break;
+          this.filter.tag = []
+          // 只获取标签列表
+          // 然后交给子组件
         }
-        case 2: {
-          this.data.codeProblemsList.list = []
-          this.subReady.codeProblemsList = false
-
-          this.params.program.tagList = tagTemp.length===0?null:tagTemp.join(',')
-          await Promise.all([
-            this.$store.dispatch('n', {
-              flag: 0,
-              method: 'get',
-              url: `/program-problems`,
-              params: {
-                page_num: this.params.program.page_num,
-                page_size: this.params.program.page_size,
-                difficult: this.params.program.difficult,
-                query: this.filter.keyword,
-                tag_list: this.params.program.tagList,
-                uid: this.$_env.testUserInfo.uid,
-              }
-            }),
-          ])
-          if (!this.$store.state.n[0].success) return
-          for (const i of this.$store.state.n[0].data.data) {
-            i._difficult = i.difficult?this.talbe.codeDifficult[i.difficult][0]:'无'
-            i._difficultFlag = i.difficult?this.talbe.codeDifficult[i.difficult][1]:'primary'
-            i._percentage = (i.ac_times / i.submit_times)
-            i._percentage = i._percentage!==NaN?'0.00':i._percentage.toFixed(2)
-          }
-          this.data.codeProblemsList.list = this.$store.state.n[0].data.data
-          this.data.codeProblemsList.total = this.$store.state.n[0].data.total
-          this.$nextTick(function () {
-            this.subReady.codeProblemsList = true
-          })
-          break;
-        }
-      
         default:
           break;
       }
@@ -466,9 +339,11 @@ export default {
     // },
     // 单标签
     async handleTagClick (data) {
+      this.filter.auxiTagList = []
       for (const i of this.filter.tag) {
         if (i.id === data.id) {
           i.isSelected = !data.isSelected
+          if (i.isSelected) this.filter.auxiTagList = [i.id]
         } else {
           i.isSelected = false
         }
---
+++ src/components/problem/completionProblems.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="completionProblems">
+    <div v-for="i in tag" :key="i">
+      {{i}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'completionProblems',
+  props: {
+    tag: Array,
+  },
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    async init () {
+			// const sleep = (ms) => {
+			// 	return new Promise(resolve => setTimeout(resolve, ms))
+      // }
+      // await sleep(1000)
+
+      // this.ready = true
+    },
+    handleSortChange () {
+
+    },
+    toProblem (data) {
+      this.$router.push({
+        name: 'problems',
+        params: {
+          id: `${data.id}`,
+        }
+      })
+    }
+  },
+  created () {
+    this.init()
+  }
+}
+</script>
+
+<style scoped>
+  .completionProblems {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
